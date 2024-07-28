007bdb51f5b61164e0560c02e5b79ed97cf958b7
Omid Jahromi <omid.esmaeelipoor@gmail.com>
2024-07-29 01:33:50 +0200

    searchbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,46 @@
 import React, { useState } from 'react';
-import { useContext } from 'react'
-import { Container, Stack, Select, Button, Text } from '@mantine/core';
-import classes from '../styles/Navbar.module.css';
+import { useContext } from 'react';
+import { useEffect } from 'react';
+import { Container, Stack, Button, Image } from '@mantine/core';
 import { Link } from 'react-router-dom';
-import { SessionContext } from '../contexts/SessionContext'
-import SignupForm from './SignupForm';
-import LoginForm from './LoginForm';
+import { SessionContext } from '../contexts/SessionContext';
+import classes from '../styles/Navbar.module.css';
 import LightDarkModeButton from './LightDarkModeButton';
+import SearchBar from './SearchBar'; // Import the SearchBar component
+import searchIcon from '../assets/images/search.png'; // Import the search icon
 
 const Navbar = () => {
-  //const [user, setUser] = useState({}) //toDo:put in context
- 
- 
+  const { isAuthenticated } = useContext(SessionContext);
+  const [products, setProducts] = useState([]);
+
+  // Fetch products data
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('http://localhost:5006/api/products');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
 
   return (
-      <Container className={classes.navbar}>
-          <Stack  position="center" spacing="sm" direction="column">
-            <Link to="/" className={classes.navLink}>Home</Link>
-            <Link to="#featured" className={classes.navLink}>Featured</Link>
-            <Link to="/about" className={classes.navLink}>About</Link>
-            <LightDarkModeButton/>
-
-        
-            {/* 
-            <Select id="#theme"
-              label="Chose color scheme"
-              placeholder="Pick value"
-              data={['light', 'dark']}
-            /> */}
-           
-           
-          </Stack >
-      </Container>
+    <Container className={classes.navbar}>
+      <Stack position="center" spacing="sm" direction="column">
+        <Link to="/" className={classes.navLink}>Home</Link>
+        <Link to="#featured" className={classes.navLink}>Featured</Link>
+        <Link to="/about" className={classes.navLink}>About</Link>
+        <LightDarkModeButton />
+        <SearchBar products={products} icon={searchIcon} /> {/* Add the SearchBar component */}
+      </Stack>
+    </Container>
   );
 };
 

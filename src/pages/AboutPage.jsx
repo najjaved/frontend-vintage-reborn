0fc5012c2bac220515a98f61667037b5556537f9
Santi Mera <santigomero@gmail.com>
2024-07-30 15:24:48 +0300

    Minor changes on about page Minor changes on about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,18 +1,21 @@
 import { Card, Title, Container, Image, Text, Badge, Group, Space } from "@mantine/core";
 import { Link } from "react-router-dom";
 import classes from '../styles/About.module.css';
+import najma from "../assets/images/Najma.jpeg";
+import omid from "../assets/images/Omid.png"
+import santi from "../assets/images/file.jpeg"
 
 const AboutPage = () => {
     return (
         <>  <Title fw={500}> The team</Title>
             <Space h="xl" />
-            <Container className={classes.aboutCards}> 
+            <Container className={classes.aboutCards}>
                 <Card shadow="sm" padding="lg" radius="md" withBorder>
                     <Card.Section>
                         <Image
-                        src="src/assets/images/file.jpeg"
-                        h="auto"
-                        alt="Santi"
+                            src={santi}
+                            h="auto"
+                            alt="Santi"
                         />
                     </Card.Section>
                     <Group mt="md" mb="xs">
@@ -22,14 +25,14 @@ const AboutPage = () => {
                     <Text>On my late 20s, nature lover and close to be FullStack Web Developer. Born and grown in Canary Islands, with experience working or studying in Great Britain, Poland and Romania. For more info click </Text>
                 </Card>
                 <Space h="md" />
-                
+
                 <Card shadow="sm" padding="lg" radius="md" withBorder>
                     <Card.Section>
                         <Image
-                        src="src/assets/images/Najma.jpeg"
-                        h="auto"
-                        padding = "sm"
-                        alt="Najma"
+                            src={najma}
+                            h="auto"
+                            padding="sm"
+                            alt="Najma"
                         />
                     </Card.Section>
                     <Group mt="md" mb="xs">
@@ -37,17 +40,17 @@ const AboutPage = () => {
                         <Badge color="blue" component={Link} href="https://de.linkedin.com/in/najaved" target="_blank" rel="noopener noreferrer">LinkedIn</Badge>
                     </Group>
                     <Text>Electrical Engineering, Masters, HiL Test Engineer, a travel
-                    enthusiast and a nature lover </Text>
+                        enthusiast and a nature lover </Text>
                 </Card>
                 <Space h="md" />
-                
+
                 <Card shadow="sm" padding="lg" radius="md" withBorder>
                     <Card.Section>
                         <Image
-                        src="src/assets/images/Omid.png"
-                        h="auto"
-                        padding = "sm"
-                        alt="Najma"
+                            src={omid}
+                            h="auto"
+                            padding="sm"
+                            alt="Najma"
                         />
                     </Card.Section>
                     <Group mt="md" mb="xs">

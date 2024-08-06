50a2ecf3094112d773d94afff244221354a7aa8b
Najma Javed <najma.javed@outlook.com>
2024-08-07 00:35:00 +0200

    minor changes in About page

--- src/pages/AboutTeamPage.jsx
+++ src/pages/AboutTeamPage.jsx
@@ -20,7 +20,7 @@ const AboutTeamPage = () => {
                     </Card.Section>
                     <Group mt="md" mb="xs">
                         <Text fw={500}>Santiago</Text>
-                        <Badge color="blue" component={Link} href="#" target="_blank" rel="noopener noreferrer">LinkedIn</Badge>
+                        <Badge color="blue" component={Link} to="#" target="_blank" rel="noopener noreferrer" className= {classes.aboutLink}>LinkedIn</Badge>
                     </Group>
                     <Text>On my late 20s, nature lover and close to be FullStack Web Developer. Born and grown in Canary Islands, with experience working or studying in Great Britain, Poland and Romania. </Text>
                 </Card>
@@ -37,7 +37,7 @@ const AboutTeamPage = () => {
                     </Card.Section>
                     <Group mt="md" mb="xs">
                         <Text fw={500}>Najma</Text>
-                        <Badge color="blue" component={Link} href="https://de.linkedin.com/in/najaved" target="_blank" rel="noopener noreferrer">LinkedIn</Badge>
+                        <Badge color="blue" component={Link} to="https://de.linkedin.com/in/najaved" target="_blank" rel="noopener noreferrer" className= {classes.aboutLink}>LinkedIn</Badge>
                     </Group>
                     <Text>Electrical Engineering, Masters, HiL Test Engineer, a travel
                         enthusiast and a nature lover </Text>
@@ -55,7 +55,7 @@ const AboutTeamPage = () => {
                     </Card.Section>
                     <Group mt="md" mb="xs">
                         <Text fw={500}>Omid</Text>
-                        <Badge color="blue" component={Link} href="https://www.linkedin.com/in/omidesmaeelipoor/" target="_blank" rel="noopener noreferrer">LinkedIn</Badge>
+                        <Badge color="blue" component={Link} to="https://www.linkedin.com/in/omidesmaeelipoor/" target="_blank" rel="noopener noreferrer" className= {classes.aboutLink}>LinkedIn</Badge>
                     </Group>
                     <Text>I am a doctoral researcher in the field of engineering from Iran. Currently, I am attending the Ironhack bootcamp to develop my skills and transition into a full-stack developer role. With a solid foundation in engineering and a passion for technology, I am excited to combine my research expertise with new programming and development skills. </Text>
                 </Card>

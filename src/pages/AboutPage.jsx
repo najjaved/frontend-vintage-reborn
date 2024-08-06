660ebd5939b1cf0008c2864ae126d09888139c42
Najma Javed <najma.javed@outlook.com>
2024-08-06 22:07:32 +0200

    added link to github repo

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,18 @@
-import { Title } from "@mantine/core";
+import { Title, Container, Anchor } from "@mantine/core";
+import { IconBrandGithub } from "@tabler/icons-react";
 
 const AboutPage = () => {
-    return ( <Title> Here details of our project</Title> );
+    return ( 
+        <Container>
+            <Title> Here details of our project</Title> 
+            <Anchor  href="https://github.com/najjaved/frontend-vintage-reborn" target="_blank">
+                <IconBrandGithub size={18} /> Source Code 
+            </Anchor>
+        
+        </Container>
+    
+
+);
 }
  
 export default AboutPage;

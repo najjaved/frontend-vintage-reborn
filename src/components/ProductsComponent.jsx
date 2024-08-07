0d472cf474b705abfdede5aecda5e1df7a53a295
Najma Javed <najma.javed@outlook.com>
2024-08-07 12:30:53 +0200

    added favourites icon to prodcuts list and details pages, functionality yet to be added

--- src/components/ProductsComponent.jsx
+++ src/components/ProductsComponent.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect, useContext } from 'react';
 import { Card, Image, Text, Badge, Button, Group, Space, SimpleGrid, AspectRatio, Skeleton } from '@mantine/core';
 import { Link } from 'react-router-dom';
 import { SessionContext } from '../contexts/SessionContext';
-//import { IconHeart } from '@tabler/icons-react'; 
 import { CartContext } from '../contexts/CartContext';
 import classes from '../styles/Products.module.css';
+import { ActionIcon } from '@mantine/core';
+import { IconHeart } from '@tabler/icons-react';
 
 const ProductsComponent = ({ onEdit }) => {
   const { fetchWithToken, isAuthenticated, user} = useContext(SessionContext);
@@ -54,7 +55,7 @@ const ProductsComponent = ({ onEdit }) => {
       ) :
       (<>
         {products.map((product) => (
-          <Card key={product._id} className= {classes.product}>
+          <Card key={product._id} className= {classes.product} >
             <Card.Section className={classes.productImage}>
               <AspectRatio ratio={1080 / 720} maw={300} mx="auto">
                 <Image
@@ -83,10 +84,13 @@ const ProductsComponent = ({ onEdit }) => {
                 View Details
               </Button>
 
+              <IconHeart variant="subtle" size={30} stroke={1.5} color="teal" />
+
               <Button variant="filled" radius="lg" onClick={() => addToCart(product)} >
                 Add to Cart {
                   calculateItemsQuantity(cartItems, product) > 0 && <> [{calculateItemsQuantity(cartItems, product)}] </>}
               </Button>
+
                           
               {isAuthenticated? (
                (user?.userId === product.createdBy || user?.role === "admin") && (
